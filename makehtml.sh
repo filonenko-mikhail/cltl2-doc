a8013ac53886d199c262d3ce53cc1c6caacb0ed8
Michael Filonenko <filonenko.mikhail@gmail.com>
2012-02-07 21:43:29 +0300

    some modification in make

--- makehtml.sh
+++ makehtml.sh
@@ -1,6 +1,6 @@
 #~/bin/sh
 mkdir -p html/
-htlatex clm.tex 'cltl,3,next,charset=utf-8' ' -cunihtf -utf8' '' '-interaction=batchmode'
+pdflatex -file-line-error -interaction nonstopmode -output-directory output clm | wc -l
 tex '\def\filename{{clm}{idx}{4dx}{ind}} \input idxmake.4ht'
 makeindex -o clm.ind clm.4dx
 htlatex clm.tex 'cltl,3,next,charset=utf-8' ' -cunihtf -utf8' '-d./html/' '-interaction=batchmode'
