d4ed225dada990fbebe023d201684466323d0e7f
Michael Filonenko <filonenko.mikhail@gmail.com>
2012-02-20 00:46:43 +0300

    add html version, translate 30% of prog.tex, refactor html tex code

--- makehtml.sh
+++ makehtml.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-mkdir -p html/
-# latex -file-line-error -interaction nonstopmode clm | wc -l
-# sleep 10
-# tex '\def\filename{{clm}{idx}{4dx}{ind}} \input idxmake.4ht'
-# makeindex -o clm.ind clm.4dx
-htlatex clm.tex 'cltl,3,next,charset=utf-8' ' -cunihtf -utf8' '-d./html/' '-interaction=batchmode'
+tex '\def\filename{{clm}{idx}{4dx}{ind}} \input idxmake.4ht'
+makeindex -o clm.ind clm.4dx
+htlatex clm.tex 'cltl,3,next,charset=utf-8' ' -cunihtf -utf8' '' '-interaction=batchmode'
