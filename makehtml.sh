13f26d26ccf2c4317a3cf47154de6d7f2148f62d
Michael Filonenko <filonenko.mikhail@gmail.com>
2012-02-22 01:56:48 +0300

    translation progress

--- makehtml.sh
+++ makehtml.sh
@@ -2,3 +2,11 @@
 tex '\def\filename{{clm}{idx}{4dx}{ind}} \input idxmake.4ht'
 makeindex -o clm.ind clm.4dx
 htlatex clm.tex 'cltl,3,next,charset=utf-8' ' -cunihtf -utf8' '' '-interaction=batchmode'
+cp -f clm.html index.html
+
+
+# \cd{( 1 2 3)}
+
+#\cd{test-function}
+
+# \cd{read}
