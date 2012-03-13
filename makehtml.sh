2fa23a76b80f33e19fd98eedec996b3a0131a99d
Michael Filonenko <filonenko.mikhail@gmail.com>
2012-03-13 16:33:29 +0300

    separated russian and english texts

--- makehtml.sh
+++ makehtml.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ -f symbols.idx ]; then
     tex '\def\filename{{symbols}{idx}{4dx}{ind}} \input  idxmake.4ht' 
-makeindex -o symbols.ind symbols.4dx 
+    makeindex -o symbols.ind symbols.4dx 
 fi
 
 if [ -f issues.idx ]; then
     tex '\def\filename{{issues}{idx}{4dx}{ind}} \input  idxmake.4ht' 
-makeindex -o issues.ind issues.4dx 
+    makeindex -o issues.ind issues.4dx 
 fi
 
 htlatex clm.tex 'cltl,3,next,charset=utf-8' ' -cunihtf -utf8' '' '-interaction=batchmode'
